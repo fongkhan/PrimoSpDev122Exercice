dc236c98c9cd6a29160f81b0b8dd3dd5de52738f
UTILISATEUR <UTILISATEUR@LAPTOP-O05VHP3O>
2022-02-22 08:59:59 +0100

    first commit

---
+++ PrimoSpDev122Exercice/src/main/java/fr/solutec/PrimoSpDev122ExerciceApplication.java
@@ -0,0 +1,13 @@
+package fr.solutec;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PrimoSpDev122ExerciceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PrimoSpDev122ExerciceApplication.class, args);
+	}
+
+}
---
+++ PrimoSpDev122Exercice/src/test/java/fr/solutec/PrimoSpDev122ExerciceApplicationTests.java
@@ -0,0 +1,13 @@
+package fr.solutec;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PrimoSpDev122ExerciceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
